469dec45822a952a8859f2496e8aa68faaa64b3e
AVasilaki <poseydon463@gmail.com>
2023-10-24 13:07:54 +0300

    upd

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,12 +1,30 @@
-import { Modal } from 'components/Modal/Modal';
-
-export const ImageGaleryItem = ({ item }) => {
-  return (
-    <>
-      <li className="gallery-item">
-        <img src={item.webformatURL} alt="" />
-      </li>
-      <Modal large={item.largeImageURL}></Modal>
-    </>
-  );
-};
+import { Modalw } from 'components/Modal/Modal';
+import { Component } from 'react';
+export class ImageGaleryItem extends Component {
+  state = {
+    isOpenModal: false,
+  };
+  onOpenModal = () => {
+    this.setState({ isOpenModal: true });
+  };
+  onCloseModal = () => {
+    this.setState({ isOpenModal: false });
+  };
+  render() {
+    const { item } = this.props;
+    return (
+      <>
+        <li className="gallery-item" onClick={this.onOpenModal}>
+          <img src={item.webformatURL} alt="" />
+        </li>
+        <Modalw
+          isOpen={this.state.isOpenModal}
+          isClose={this.onCloseModal}
+          large={item.largeImageURL}
+        ></Modalw>
+        {/* <Modal large={item.largeImageURL}></Modal> */}
+      </>
+    );
+  }
+}
+// export const ImageGaleryItem = ({ item }) => {};
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,44 @@
-import * as basicLightbox from 'basiclightbox';
-export const Modal = ({ large }) => {
-  const instance = basicLightbox.create(`
-    <div class="overlay">
-  <div class="modal">
-    <img src="${large}" alt="" />
-  </div>
-</div>
-`);
-
-  instance.show();
+import Modal from 'react-modal';
+import '../Modal/Modal.css';
+export const Modalw = ({ isOpen, large, isClose }) => {
+  console.log(isOpen);
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+  Modal.setAppElement('#root');
+  return (
+    <Modal
+      isOpen={isOpen}
+      // onAfterOpen={afterOpenModal}
+      onRequestClose={isClose}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <div className="Overlay">
+        <div className="Modal">
+          <img src={large} alt="" />
+        </div>
+      </div>
+    </Modal>
+  );
 };
+
+// import * as basicLightbox from 'basiclightbox';
+// export const Modal = ({ large }) => {
+//   const instance = basicLightbox.create(`
+//     <div class="overlay">
+//   <div class="modal">
+//     <img src="${large}" alt="" />
+//   </div>
+// </div>
+// `);
+
+//   instance.show();
+// };
