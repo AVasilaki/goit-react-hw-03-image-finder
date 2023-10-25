d5b30b5749c24ed8853238bc201b56bad04a17df
AVasilaki <poseydon463@gmail.com>
2023-10-25 18:36:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,7 @@ export class App extends Component {
     this.setState(prevState => ({
       page: (prevState.page += 1),
     }));
+    console.log(this.state.page);
   };
 
   onChange = evt => {
